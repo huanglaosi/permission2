3dbf43cef65b2e3aa3987bcd37b78bb0232760a7
huanglaosi <13104871849@163.com>
2018-09-08 11:08:55 +0800

    Initial commit

---
+++ src/main/java/com/mmall/service/SysUserService.java
@@ -0,0 +1,98 @@
+package com.mmall.service;
+
+import com.google.common.base.Preconditions;
+import com.mmall.beans.PageQuery;
+import com.mmall.beans.PageResult;
+import com.mmall.common.RequestHolder;
+import com.mmall.dao.SysUserMapper;
+import com.mmall.exception.ParamException;
+import com.mmall.model.SysUser;
+import com.mmall.param.UserParam;
+import com.mmall.util.BeanValidator;
+import com.mmall.util.IpUtil;
+import com.mmall.util.MD5Util;
+import com.mmall.util.PasswordUtil;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class SysUserService {
+
+    @Resource
+    private SysUserMapper sysUserMapper;
+
+    public void save(UserParam param){
+        BeanValidator.check(param);
+        if(checkTelephoneExist(param.getTelephone(), param.getId())){
+            throw new ParamException("电话已被占用");
+        }
+        if(checkEmailExist(param.getMail(), param.getId())){
+            throw new ParamException("邮箱已被占用");
+        }
+        String password = PasswordUtil.randomPassword();
+        //TODO:
+        password = "12345678";//将生成的初始密码通过邮件发给用户，通知用户进行修改
+        String encryptedPassword = MD5Util.encrypt(password);
+        SysUser after = SysUser.builder().username(param.getUsername()).telephone(param.getTelephone()).mail(param.getMail())
+                .password(password).deptId(param.getDeptId()).status(param.getStatus()).remark(param.getRemark()).build();
+        after.setOperator(RequestHolder.getCurrentUser().getUsername());
+        after.setOperateIp(IpUtil.getRemoteIp(RequestHolder.getCurrentRequest()));
+        after.setOperateTime(new Date());
+
+        // TODO: sendEmail
+
+        sysUserMapper.insertSelective(after);
+
+
+    }
+
+    public void update(UserParam param){
+        BeanValidator.check(param);
+        if(checkTelephoneExist(param.getTelephone(), param.getId())){
+            throw new ParamException("电话已被占用");
+        }
+        if(checkEmailExist(param.getMail(), param.getId())){
+            throw new ParamException("邮箱已被占用");
+        }
+        SysUser before = sysUserMapper.selectByPrimaryKey(param.getId());
+        Preconditions.checkNotNull(before, "待更新的用户不存在");
+        SysUser after = SysUser.builder().id(param.getId()).username(param.getUsername()).telephone(param.getTelephone()).mail(param.getMail())
+                .deptId(param.getDeptId()).status(param.getStatus()).remark(param.getRemark()).build();
+        after.setOperator(RequestHolder.getCurrentUser().getUsername());
+        after.setOperateIp(IpUtil.getRemoteIp(RequestHolder.getCurrentRequest()));
+        after.setOperateTime(new Date());
+
+        sysUserMapper.updateByPrimaryKeySelective(after);
+    }
+
+    public boolean checkEmailExist(String mail, Integer userId){
+        return sysUserMapper.countByMail(mail, userId) > 0;
+    }
+
+    public boolean checkTelephoneExist(String telephone, Integer userId){
+        return sysUserMapper.countByTelephone(telephone, userId) > 0;
+    }
+
+    public SysUser findByKeyword(String keyword){
+        return sysUserMapper.findByKeyword(keyword);
+    }
+
+
+
+    public PageResult<SysUser> getPageByDeptId(int deptId, PageQuery page) {
+        BeanValidator.check(page);
+        int count = sysUserMapper.countByDeptId(deptId);
+        if (count > 0) {
+            List<SysUser> list = sysUserMapper.getPageByDeptId(deptId, page);
+            return PageResult.<SysUser>builder().total(count).data(list).build();
+        }
+        return PageResult.<SysUser>builder().build();
+    }
+
+    public List<SysUser> getAll() {
+        return sysUserMapper.getAll();
+    }
+}
